79c2f7088317a5f5e77657e01cafd1248a91ecde
Evan Stoner <e.l.stoner@gmail.com>
2013-09-14 11:03:15 -0400

    A virtual keyboard is displayed. Snippets can be set and used. Removed Bootstrap.

--- js/content_script.js
+++ js/content_script.js
@@ -3,12 +3,17 @@ var lastInput;
 $(function() {
   $("input[type=text], textarea").focus(function() {
     lastInput = this;
-    console.log(lastInput);
   });
 });
   
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  $(lastInput).val(request.text);
+  var text;
+  if (request.type == "add") {
+    text = $(lastInput).val() + request.text;
+  } else if (request.type == "set") {
+    text = request.text;
+  }
+  $(lastInput).val(text);
   sendResponse({response: "ok"});
 });
--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,61 @@
+var keys = {};
+
+
 $(function() {
-  $("#textbox").focus().keyup(textbox_change);
+  for (var i = 65; i <= 90; i++) {
+    keys[i] = String.fromCharCode(i);
+  }
+  $(".key").each(function() {
+    var letter = $(this).attr("id");
+    var $key = $(this);
+    var snippet = chrome.storage.local.get(letter, function(item) {
+      var snippet = item[letter];
+      if (snippet) {
+        console.log("Found snippet for E: " + snippet);
+        $key.children(".snippet").text(snippet);
+      }
+    });
+    $key.children(".letter").text(letter);
+  });
+  
+  $("body").keydown(keydown).keyup(keyup);
+  $("#setSnippet").click(setSnippet);
 });
 
-
-function textbox_change(e) {
-  var text = $(this).val();
-  $("#snippets").html(text);
+function keydown(e) {
+  var text = $("#" + keys[e.which]).children(".snippet").text();
+  if (!text) {
+    return;
+  }
+  $("#" + text).addClass("key-active");
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    console.log(tabs[0].id);
-    chrome.tabs.sendMessage(tabs[0].id, {text: text}, function(response) {
+    chrome.tabs.sendMessage(tabs[0].id, {type: "add", text: text}, function(response) {
       console.log(response);
     });
   });
+}
+
+
+function keyup(e) {
+  $("#" + keys[e.which]).removeClass("key-active");
+}
+
+
+function setSnippet() {
+  var letter = $("#letter").val().toUpperCase();
+  var snippet = $("#snippet").val();
+  if (snippet) {
+    var setting = {};
+    setting[letter] = snippet;
+    chrome.storage.local.set(setting, function() {
+      $("#letter").val("");
+      $("#snippet").val("");
+      $("#" + letter).children(".snippet").text(setting[letter]);
+    });
+  } else {
+    chrome.storage.local.remove(letter, function() {
+      $("#letter").val("");
+      $("#" + letter).children(".snippet").text("");
+    });
+  }
 }
